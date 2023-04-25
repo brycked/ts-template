af12495be4039b355ef3239f80422e825f456ceb
Brycked <brycked@proton.me>
2023-04-25 21:40:39 +0200

    chore(build): configure explicitely

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  tsconfig: "src/tsconfig.json",
   entry: ["src/**/*.ts"],
+  format: ["cjs"],
+  target: "es2020",
+  clean: true,
+  bundle: true,
+  skipNodeModulesBundle: true,
+  treeshake: true,
   sourcemap: true,
   dts: true,
-  tsconfig: "src/tsconfig.json",
-  clean: true,
 });
